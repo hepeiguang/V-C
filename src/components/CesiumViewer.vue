00aab0ea6d6c5c30294cd37d01542f52732ae1ce
hepeiguang <1683325202@qq.com>
2019-08-05 18:31:47 +0800

    up

---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+<script type="text/javascript">
+import Cesium from 'cesium/Cesium'
+import widgets from 'cesium/Widgets/widgets.css'
+
+export default {
+  name: 'CesiumViewer',
+  mounted () {
+    var viewer = new Cesium.Viewer('cesiumContainer');
+  }
+}
+
+</script>
+<style  rel="stylesheet/css" lang="css" scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
